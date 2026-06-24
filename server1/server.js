fce1091146b7109ea7e5c4150669e221edc49d9f
Manjeet <manjeetyadav90698m@gmail.com>
2026-06-24 18:55:14 +0530

    Fix Render port

--- server1/server.js
+++ server1/server.js
@@ -1,5 +1,4 @@
-require("dotenv").config();
-const dotenv = require("dotenv");
+;
 const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
@@ -10,7 +9,7 @@ const issueRoutes = require("./routes/issueRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
-dotenv.config();
+require("dotenv").config();
 connectDB();
 app.use(cors());
 app.use(express.json());
@@ -25,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server Running On Port ${PORT}`);
